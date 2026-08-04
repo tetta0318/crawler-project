b2fcd86eb2866d86e43c61857599290699a66120
0318tetuo <0318tetuo@gmail.com>
2026-08-04 21:02:44 +0900

    READMEを追加

--- app/src/main/java/crawler/CrawlerMain.java
+++ app/src/main/java/crawler/CrawlerMain.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 public class CrawlerMain {
   //リソースをダウンロードするスレッドの数
-  private static final int THREAD_COUNT = 50;
+  private static final int THREAD_COUNT = 16;
 
   public static void main(String @NonNull [] args){
     String targetUrlStr = args[0];
@@ -23,6 +23,7 @@ public class CrawlerMain {
       DirCreater.createDir(outputRoot);
     } catch (IOException e) {
       Logger.error("ディレクトリが作成できませんでした");
+      return;
     }
 
     ExecutorService pool = Executors.newFixedThreadPool(THREAD_COUNT);
